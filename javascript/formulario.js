2f1bee6cfeec11235062498df0d0fc3ad7b4684a
nachocastagna <nacho_castagna@hotmail.com>
2022-09-16 10:06:06 -0300

    Eventosenformulario

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -5,30 +5,41 @@ formulario.addEventListener("submit", (e) => {
   e.preventDefault();
   let inputs = e.target.children;
 
-  if(!inputs[4].value.includes("@")){
+  const nombre = inputs[0].value ;
+  const raza = inputs[1].value ;
+  const tipo = inputs[2].value ;
+  const color = inputs[3].value ;
+  const telefono = inputs[4].value ;
+  const email = inputs[5].value ;
+  const provincia = inputs[6].value ;
+  const barrio = inputs[7].value ;
+
+  let condicion = nombre && raza && tipo && color && telefono && email && provincia && barrio;
+
+  if(condicion === ""){
     contenedor.innerHTML = "";
     let div = document.createElement("div");
-    div.innerHTML = "Email no válido";
-    div.className = "rojo";
+    div.innerHTML = "Falta completar informacíon";
+    div.className = "letraAlerta";
     contenedor.append(div);
     console.log(inputs);
   }else{
     contenedor.innerHTML = "";
-}
-}
+    let div = document.createElement("div");
+    div.innerHTML = "Animal agregado con exito";
+    div.className = "letraAgregado";
+    contenedor.append(div);
+    console.log(inputs);
+    }
+  }
 );
 
-function envioFormulario(){
-    let animal = document.querySelector('#animales');
+
+/* function envioFormulario(){
+    let animal = document.getElementById("animales");
     let infomacionRecopilada = document.getElementsByClassName('texto');
-    agregado = [];
+    const agregado = [];
     for(let i = 0; i < infomacionRecopilada.length; i++){
-        agregado[i] = infomacionRecopilada[i].value;
-        animal.innerHTML = `<p>${agregado}</p>`
-    }
-
-
-}
-
-
-
+        agregado[i] = ` ${infomacionRecopilada[i].value}  `;
+      }
+        animal.innerHTML += ` ${agregado}<br>`}   */
