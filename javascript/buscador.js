97ab41d4cad809185c2c76d0e1a61eb77d2eeee0
nachocastagna <nacho_castagna@hotmail.com>
2022-10-24 10:13:19 -0300

    pendiente eliminar al tocar boton cards, y que funcione en todas las cards

--- javascript/buscador.js
+++ javascript/buscador.js
@@ -4,7 +4,7 @@ let animalesCargados = JSON.parse(localStorage.getItem("AnimalesGuardados"));
 
 for(i = 0; i < animalesCargados.length; i++){
   const card = `<div class="card mx-5 my-5" style="width: 18rem;">
-  <img src="https://arbolabc.nyc3.cdn.digitaloceanspaces.com/Science/animals/articles/animales-domesticos/cat.jpg" class="card-img-top h-100" alt="...">
+  <img src="https://www.vetcomunicaciones.com.ar/uploadsfotos/animales_domesticos.jpg" class="card-img-top h-100" alt="...">
   <div class="card-body">
     <h5 class="card-title">${animalesCargados[i].nombre} / ${animalesCargados[i].raza} / ${animalesCargados[i].color}</h5>
     <p class="card-text"></p>
@@ -15,6 +15,7 @@ for(i = 0; i < animalesCargados.length; i++){
 
 const cardAnimales = document.getElementById("card-conteiner");
 cardAnimales.innerHTML+=card
+
 };
 
     let encontrado = document.getElementById("encontrado");  
