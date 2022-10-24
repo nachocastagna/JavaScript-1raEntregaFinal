bec2dec0f18fc8af6a851bc4b5c35a814dcace87
nachocastagna <nacho_castagna@hotmail.com>
2022-10-23 22:46:24 -0300

    Se usa localStorage para agregar animales

--- javascript/buscador.js
+++ javascript/buscador.js
@@ -1,27 +1,54 @@
 //PRACTICA CON LOCALSTORAGE
 
-/* let animalesCargados = JSON.parse(localStorage.getItem("AnimalesGuardados"));
+let animalesCargados = JSON.parse(localStorage.getItem("AnimalesGuardados"));
 
 for(i = 0; i < animalesCargados.length; i++){
   const card = `<div class="card mx-5 my-5" style="width: 18rem;">
   <img src="https://arbolabc.nyc3.cdn.digitaloceanspaces.com/Science/animals/articles/animales-domesticos/cat.jpg" class="card-img-top h-100" alt="...">
   <div class="card-body">
     <h5 class="card-title">${animalesCargados[i].nombre} / ${animalesCargados[i].raza} / ${animalesCargados[i].color}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Contactar al dueño</a>
-    <a href="#" class="btn btn-primary">Encontrado</a>
+    <p class="card-text"></p>
+    <button id="encontrado" class="btn btn-primary">Contactar al dueño</button>
+    <button id="eliminar" class="btn btn-primary">Encontrado</button>
   </div>
 </div>`
 
 const cardAnimales = document.getElementById("card-conteiner");
 cardAnimales.innerHTML+=card
+let encontrado = document.getElementById("encontrado");  
 
-}; */
+    encontrado.addEventListener("click", function(){
+    let animalesCargados = JSON.parse(localStorage.getItem("AnimalesGuardados"));
+    Swal.fire(animalesCargados)});
+
+
+
+    let eliminar = document.getElementById("eliminar");
+    eliminar.addEventListener("click", function(){  
+    Swal.fire({
+      title: 'Quieres eliminar de la lista de animales perdidos?',
+      text: "Si eliminas, daremos por encontrado el animal",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, lo encontre!'
+      }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Animal eliminado!',
+          'Estamos muy felices',
+          'success'
+          )}})
+        });
+
+  };
+;
 
 
 //Fetch
 
-const traerDatos = async () => {
+/* const traerDatos = async () => {
   const lista = document.getElementById("card-conteiner");
   
   try {
@@ -75,5 +102,5 @@ const traerDatos = async () => {
   }
 };
 
-traerDatos();
+traerDatos(); */
 
--- javascript/formulario.js
+++ javascript/formulario.js
@@ -2,19 +2,17 @@ let contenedor = document.getElementById("contenedor"); //trae desde HTML id "co
 let formulario = document.getElementById("formulario");//trae desde HTML id "formulario"
 let animales = document.getElementById("animales");//trae desde HTML id "animales"
 
-const animalesArray = [] // array o arreglo
+
+const animalesArray = [];
+// array o arreglo
 
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
   let inputs = e.target.children;
-
   const nombre = inputs[0].value ;
   const raza = inputs[1].value ;
-  //const tipo = inputs[2].value ;
   const color = inputs[2].value ;
   const telefono = inputs[3].value ;
- // const email = inputs[5].value ;
- // const provincia = inputs[6].value ;
   const barrio = inputs[4].value ;
 
  let condicion = nombre && raza && color && telefono && barrio; // Condicion para agregar cartel si falta completar informacion en formulario
@@ -35,28 +33,31 @@ formulario.addEventListener("submit", (e) => {
       timer: 2500
       })
 
-    class Animal{ //clase generadora de animales con estas caracteristicas
+  
+  class Animal{ //clase generadora de animales con estas caracteristicas
       constructor(nombre, raza, color, telefono, barrio){
         this.nombre =  nombre;
         this.raza = raza;
-        //this.tipo = tipo;
         this.color = color;
         this.telefono = telefono;
-       // this.email = email;
-        //this.provincia = provincia;
         this.barrio = barrio;
+
       }
     }     
-
+    
     let agregar = new Animal (nombre, raza, color, telefono, barrio);
 
     animalesArray.push(agregar); // se agrega animal al arreglo o array
 
     localStorage.setItem("AnimalesGuardados", JSON.stringify(animalesArray));
-    
+        
   }
 }
 );
+
+
+
+
 
 /* function getAnimalesArray() {
   if(localStorage.setItem("AnimalesGuardados", JSON.stringify(animalesArray))  == null){
